6f551eb0b5fbb0e9513d98bbd07894a0112de08c
ashiqcsebu <ashiq.cse3.bu@gmail.com>
2022-11-15 03:27:22 +0600

    Doctors Appointment partially Completed

--- src/Route/Routes/Route.js
+++ src/Route/Routes/Route.js
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Main from '../../Layout/Main';
+import Appointment from '../../Pages/Appointment/Appointment/Appointment';
 import Home from '../../Pages/Home/Home/Home';
+
 import Login from '../../Pages/Login/Login';
 
 export const router = createBrowserRouter([
@@ -16,7 +18,11 @@ export const router = createBrowserRouter([
             {
                 path: '/login',
                 element: <Login></Login>
-            }
+            },
+            {
+                path: '/appointment',
+                element: <Appointment></Appointment>
+            },
          ]
 
         }
