b254b775d9a131b06b908865c9258a5371fb92c9
ashiqcsebu <ashiq.cse3.bu@gmail.com>
2022-11-24 14:47:38 +0600

    daisy ui added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './Route/Routes/Route';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
     <div className="max-w-[1440px] mx-auto">
       <RouterProvider router={router}></RouterProvider>
+      <Toaster></Toaster>
     </div>
   );
 }
